c60cd9c121ffa1a8e4a9613d2c53ceaf72164627
akshaykanherkar7 <akshaykanherkar7@gmail.com>
2022-06-30 21:01:31 +0530

    Day8 Custome Hooks useFetch and useTimeout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import useFetch from "./Hooks/useFetch";
+import useTimeout from "./Hooks/useTimeout";
 
 function App() {
+  const ready1 = useTimeout(2000);
+  const ready2 = useTimeout(5000);
+
+  const [query, setQuery] = useState("");
+  const { data, loading, error } = useFetch(
+    `https://api.github.com/search/users?q=${query || "masai"}`
+  );
+  console.log("data:", data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ display: "flex", justifyContent: "space-around" }}>
+        <div>
+          <h1>useTimeout</h1>
+          {ready1 ? "Ready1" : "Not-Ready1"}
+          <br />
+          {ready2 ? "Ready2" : "Not-Ready2"}
+        </div>
+
+        <div>
+          <h1>useFetch</h1>
+          <input
+            type="text"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          {loading && "Loading"}
+          {data.length > 0 &&
+            data.map((item) => {
+              return <div key={item.id}>{item.login}</div>;
+            })}
+        </div>
+      </div>
     </div>
   );
 }
